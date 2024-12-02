2f42bce324821e1cfd21701b0f4f85f1ba25051d
Zdravko Dimov <148683682+Zdrave88@users.noreply.github.com>
2024-12-02 18:25:53 +0200

    Add files via upload

---
+++ 02. 01. Basic Syntax, Conditional Statements and Loops - Exercises/E01.Ages/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ •	0-2 – baby
+•	3-13 – child
+•	14-19 – teenager
+•	20-65 – adult
+•	>= 66 – elder
+
+ */
+
+int ageInput = int.Parse(Console.ReadLine());
+string result = "";
+
+if (ageInput >= 0 && ageInput <= 2)
+{
+    result = "baby";
+}
+else if (ageInput >= 3 && ageInput <= 13)
+{
+    result = "child";
+}
+else if (ageInput >= 14 && ageInput <= 19)
+{
+    result = "teenager";
+}
+else if (ageInput >= 20 && ageInput <= 65)
+{
+    result = "adult";
+}
+else if (ageInput >= 66)
+{
+    result = "elder";
+}
+else
+{
+    Console.WriteLine("error");
+}
+Console.WriteLine(result);
---
+++ 02. 01. Basic Syntax, Conditional Statements and Loops - Exercises/E03.Vacation/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ •	A count of people who are going on vacation.
+•	Type of the group (Students, Business or Regular).
+•	The day of the week which the group will stay on (Friday, Saturday or Sunday).
+ */
+
+int peopleAmount = int.Parse(Console.ReadLine());
+string groupType = Console.ReadLine();
+string day = Console.ReadLine();
+
+double price = 0.0;
+switch (groupType)
+{
+    case "Students":
+        if (day == "Friday")
+        {
+            price = 8.45;
+        }
+        else if (day == "Saturday")
+        {
+            price = 9.80;
+        }
+        else
+        {
+            price = 10.46;
+        }
+        break;
+    case "Business":
+        if (day == "Friday")
+        {
+            price = 10.90;
+        }
+        else if (day == "Saturday")
+        {
+            price = 15.60;
+        }
+        else
+        {
+            price = 16;
+        }
+        break;
+    case "Regular":
+        if (day == "Friday")
+        {
+            price = 15;
+        }
+        else if (day == "Saturday")
+        {
+            price = 20;
+        }
+        else
+        {
+            price = 22.50;
+        }
+        break;
+}
+double totalPrice = peopleAmount * price;
+
+if (groupType == "Students" && peopleAmount >= 30)
+{
+    totalPrice *= 0.85;
+}
+if (groupType == "Business" && peopleAmount >= 100)
+{
+    totalPrice -= 10 * price;
+}
+if (groupType == "Regular" && peopleAmount >= 10 && peopleAmount <= 20)
+{
+    totalPrice *= 0.95;
+}
+Console.WriteLine($"Total price: {totalPrice:f2}");
